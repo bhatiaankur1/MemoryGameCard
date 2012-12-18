2c3810eac26310ee7a7e20266156a76dd632c497
Ankur Bhatia <rubyuser@rubyuser-VirtualBox.(none)>
2012-12-18 12:47:51 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memorygamecard::Application.config.secret_token = 'f10aa63972c9f2b71745bea43c1ad653ae07277e8ea03d01eaf09f47cb90b4a3ba23f4f10ffabb26ed044bfd2164b558a76f675c878f4b539234be1783568f6b'
