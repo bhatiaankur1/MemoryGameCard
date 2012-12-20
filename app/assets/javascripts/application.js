1e109e236d0715890214edf2d6b40ec45f95c4e7
Ankur Bhatia <rubyuser@rubyuser-VirtualBox.(none)>
2012-12-20 16:17:08 +0530

    Images and a little code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,33 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+function showimage(but) {
+if (oktoclick) {
+oktoclick = false; 
+document.f[('img'+but)].src = 'image'+map[but]+'.gif';
+if (ctr == 0) {
+ctr++;
+clickarray[0] = but;
+oktoclick = true;
+} else {
+clickarray[1] = but;
+ctr = 0;
+setTimeout('returntoold()', 600);
+      }
+   }
+}
+$(document).ready(function() {
+
+    $(function() {
+    	  $("#attemp").html("Your Attempts ---> "+attempts.toString());
+        });
+    $(".tag").on('click', function(showimg){
+    	attempts = attempts + 1;
+    	id = $(this).attr("id").slice(3)
+		src = "/assets/Img"+$.trim(map[id])+".jpg"
+    	$("#"+id).attr("src",src);
+    	$("#attemp").html("Your Attempts ---> "+attempts.toString());
+        });
+    });
